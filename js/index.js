d2886e1a3e4cfe7e8857387f5d4c157a996733e2
gsbka7 <95899871+gsbka7@users.noreply.github.com>
2022-04-27 00:17:41 +0800

    upload

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
+$(".navbar-ham").click(function () {
+    $('#contentId').toggleClass('active');
+    $("#meunId").toggleClass('active');
+}); 
+
 // const meun = document.querySelector('.navbar-ham')
 // const ham = document.querySelector('.content')
-
 
 // meun.addEventListener('click', function () {
 //     document.getElementById("meunId").classList.add('active')
@@ -9,17 +13,55 @@
 //     setTimeout(function () {
 //         meun.setAttribute("style","display: none");}, 500);
 
-
 //     setTimeout(function () {
 //          ham.setAttribute("style","transform: scale(50)");}, 500);
 //         }
-         
 // )
 
-$(".navbar-ham").click(function () {
-    $('#contentId').toggleClass('active');
-    $("#meunId").toggleClass('active');
-}); 
+const infoElement = document.querySelector('.info')
+const infoDistance = infoElement.getBoundingClientRect().top
+if(infoDistance <= 100){
+    infoElement.classList.add('show')
+}
+
+const aboutContentElement = document.querySelector('.about .content')
+aboutContentDistence = aboutContentElement.getBoundingClientRect().top
+if(aboutContentDistence <= 1118){
+    aboutContentElement.classList.add('show')
+}
+
+const titleElement = document.querySelectorAll('.title')
+const expElement = document.querySelectorAll('.exp')
+const skillElement = document.querySelector('.skill')
+const protfolipElement = document.querySelector('.protfolio')
+
+document.addEventListener('scroll',function() {
+    for (let i = 1; i < titleElement.length; i++) {
+        const titleDistance = titleElement[i].getBoundingClientRect().top + titleElement[i].clientHeight * 0.5;
+        if(window.innerHeight > titleDistance){
+            titleElement[i].classList.add('show')
+        }
+    }
+    for (let i = 0; i < expElement.length; i++){
+        const expElementDistence = expElement[i].getBoundingClientRect().top + expElement[i].clientHeight * 0.3
+        if(window.innerHeight > expElementDistence){
+            expElement[i].classList.add('show')
+        }
+    }
+    
+    const skillElementDistance = skillElement.getBoundingClientRect().top + skillElement.clientHeight * 0.5
+    if(window.innerHeight > skillElementDistance){
+        skillElement.classList.add('show')
+    }
+
+    const protfolioElementDistance = protfolipElement.getBoundingClientRect().top + protfolipElement.clientHeight * 0.15
+    if(window.innerHeight > protfolioElementDistance){
+        protfolipElement.classList.add('show')
+    }
+})
+
+
+
 
 
 
